9ec206c7012b9333db68d5213e3ed78885431158
samir3112 <samirparate3112@gmail.com>
2025-07-05 14:42:39 +0000

    new start

--- infra/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "public_ip" {
-  value = aws_instance.contact_api.public_ip
-}
--- init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum update -y
-amazon-linux-extras install docker -y
-service docker start
-usermod -a -G docker ec2-user
-docker pull samir3112/contact-api
-docker run -d -p 80:5000 samir3112/contact-api
