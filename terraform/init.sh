75e7d7e556f2d21b8a74669d6d2e3bbce6608d39
samir3112 <samirparate3112@gmail.com>
2025-07-17 09:48:19 +0000

    new

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 yum update -y
-amazon-linux-extras install docker -y
-service docker start
-usermod -a -G docker ec2-user
+yum install -y docker
+systemctl start docker
+systemctl enable docker
+usermod -aG docker ec2-user
 docker pull samir3112/contact-api
 docker run -d -p 80:5000 samir3112/contact-api
+
+# terraform init  # terraform plan  # terraform apply -auto-approve  # terraform output
+# chmod 400 ~/n-varginia.pem
+# ssh -i ~/n-varginia.pem ec2-user@<public-ip>
+# docker ps
+# curl http://<public-ip> and curl http://<public-ip>/contacts
